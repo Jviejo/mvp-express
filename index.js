8d0b9eee102b0fd857053d5fec9765a049a82028
jose viejo <jviejo.devtoday@gmail.com>
2023-08-13 11:53:59 +0200

    cambios

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!!!!');
 })
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!!!!!!');
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+app.listen(port);
